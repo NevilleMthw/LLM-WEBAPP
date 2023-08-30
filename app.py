4a44d5c5a554afd41830a26523d67d4828a5966d
nevillemthw <nevillemathew2014@gmail.com>
2023-08-30 11:00:43 +0400

    Reformatted code and used BERT for Q&A

--- app.py
+++ app.py
@@ -1,25 +1,41 @@
 from flask import Flask, render_template, request, jsonify
-from transformers import GPT2LMHeadModel, GPT2Tokenizer, AutoTokenizer, AutoModelForCausalLM
+from transformers import (
+    GPT2LMHeadModel,
+    GPT2Tokenizer,
+    AutoTokenizer,
+    AutoModelForCausalLM,
+    DistilBertTokenizer,
+    DistilBertForQuestionAnswering,
+)
+import torch
 
 app = Flask(__name__)
 
-@app.route('/')
+tokenizer = DistilBertTokenizer.from_pretrained('distilbert-base-uncased-distilled-squad')
+model = DistilBertForQuestionAnswering.from_pretrained('distilbert-base-uncased-distilled-squad')
+
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.route('/generate_response', methods=['POST'])
+
+@app.route("/generate_response", methods=["POST"])
 def generate_response():
-    user_input = request.json['user_input']
+    user_input = request.json["user_input"]
+    # context = "Extractive Question Answering is the task of extracting an answer from a text given a question. An example of a question answering dataset is the SQuAD dataset, which is entirely based on that task. If you would like to fine-tune a model on a SQuAD task, you may leverage the examples/pytorch/question-answering/run_squad.py script."  # Provide relevant context here.
+    context1 = "Albert Einstein, the renowned physicist, was born on March 14, 1879, in Ulm, Germany."
 
-    model_name = "distilgpt2"
-    tokenizer = AutoTokenizer.from_pretrained(model_name)
-    inputs = tokenizer(user_input, return_tensors="pt").input_ids
-    model = AutoModelForCausalLM.from_pretrained(model_name)
-    outputs = model.generate(inputs, max_new_tokens=100, do_sample=True, top_k=50, top_p=0.95)
-    generated_text = tokenizer.batch_decode(outputs, skip_special_tokens=True)
-
-    return jsonify({'response': generated_text})
-
-if __name__ == '__main__':
-    app.run(debug=True)
+    inputs = tokenizer.encode_plus(user_input, context1, return_tensors="pt")
+    with torch.no_grad():
+        outputs = model(**inputs)
     
+    answer_start_index = torch.argmax(outputs.start_logits)
+    answer_end_index = torch.argmax(outputs.end_logits)
+    
+    answer_tokens = inputs.input_ids[0, answer_start_index : answer_end_index + 1]
+    generated_text = tokenizer.decode(answer_tokens, skip_special_tokens=True)
+
+    return jsonify({"response": generated_text})
+
+if __name__ == "__main__":
+    app.run(debug=True)
